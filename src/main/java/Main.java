c90b9e467f55eb027dda5c3fe75b7de01843422c
Богдан Демьянчук <t0974447144@gmail.com>
2020-05-26 19:28:56 +0300

    add ShoppingCart and Ticket

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,42 +1,38 @@
 import com.cinema.exeption.AuthenticationException;
+import com.cinema.exeption.DataProcessingException;
 import com.cinema.lib.Injector;
 import com.cinema.model.CinemaHall;
 import com.cinema.model.Movie;
 import com.cinema.model.MovieSession;
+import com.cinema.model.Ticket;
 import com.cinema.model.User;
 import com.cinema.service.AuthenticationService;
 import com.cinema.service.CinemaHallService;
 import com.cinema.service.MovieService;
 import com.cinema.service.MovieSessionService;
+import com.cinema.service.ShoppingCartService;
+import com.cinema.service.UserService;
+import com.cinema.util.HibernateUtil;
+import org.hibernate.Session;
+import org.hibernate.query.Query;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
+import java.util.List;
 
 public class Main {
     private static Injector injector = Injector.getInstance("com.cinema");
 
     public static void main(String[] args) throws AuthenticationException {
-        Movie movie = new Movie();
-        movie.setTitle("Eralash");
-        movie.setDescription("Season 1");
-        MovieService movieService = (MovieService) injector.getInstance(MovieService.class);
-        movieService.add(movie);
-        System.out.println(movieService.getAll());
-        CinemaHallService cinemaHallService = (CinemaHallService)
-                injector.getInstance(CinemaHallService.class);
-        CinemaHall cinemaHall = new CinemaHall();
-        cinemaHall.setCapacity(31);
-        cinemaHall = cinemaHallService.add(cinemaHall);
-        System.out.println(cinemaHallService.getAll());
-        MovieSession movieSession = new MovieSession();
-        movieSession.setCinemaHall(cinemaHall);
-        movieSession.setMovie(movie);
-        movieSession.setShowTime(LocalDateTime.of(LocalDate.now(), LocalTime.of(14, 30)));
-        MovieSessionService movieSessionService = (MovieSessionService)
-                injector.getInstance(MovieSessionService.class);
-        movieSessionService.add(movieSession);
-        System.out.println(movieSessionService.findAvailableSessions(movie.getId(),
-                LocalDate.now()));
+        checkMovie();
+        checkUser();
+        checkCinemaHall();
+        checkMovieSession();
+        checkShoppingCartService();
+        checkTicket();
+    }
+
+    private static void checkUser() throws AuthenticationException {
         AuthenticationService authenticationService = (AuthenticationService)
                 injector.getInstance(AuthenticationService.class);
         for (int i = 0; i < 10; i++) {
@@ -44,5 +40,76 @@ public class Main {
             authenticationService.register(user.getEmail(), user.getPassword());
         }
         System.out.println("Get 3rd user -" + authenticationService.login("email2@com", "*2"));
+    }
+
+    private static void checkMovie() {
+        Movie movie = new Movie();
+        movie.setTitle("Eralash");
+        movie.setDescription("Season 1");
+        MovieService movieService = (MovieService) injector.getInstance(MovieService.class);
+        movieService.add(movie);
+        System.out.println(movieService.getAll());
+    }
+
+    private static void checkCinemaHall() {
+        CinemaHallService cinemaHallService = (CinemaHallService)
+                injector.getInstance(CinemaHallService.class);
+        CinemaHall cinemaHall = new CinemaHall();
+        cinemaHall.setCapacity(31);
+        cinemaHallService.add(cinemaHall);
+        System.out.println(cinemaHallService.getAll());
+    }
+
+    private static void checkMovieSession() {
+        MovieSession movieSession = new MovieSession();
+        CinemaHallService cinemaHallService = (CinemaHallService)
+                injector.getInstance(CinemaHallService.class);
+        movieSession.setCinemaHall(cinemaHallService.getAll().get(0));
+        MovieService movieService = (MovieService) injector.getInstance(MovieService.class);
+        Movie movie = movieService.getAll().get(0);
+        movieSession.setMovie(movie);
+        movieSession.setShowTime(LocalDateTime.of(LocalDate.now(), LocalTime.of(14, 30)));
+        MovieSessionService movieSessionService = (MovieSessionService)
+                injector.getInstance(MovieSessionService.class);
+        movieSessionService.add(movieSession);
+        System.out.println(movieSessionService.findAvailableSessions(movie.getId(),
+                LocalDate.now()));
+    }
+
+    private static void checkShoppingCartService() {
+        ShoppingCartService shoppingCartService = (ShoppingCartService)
+                injector.getInstance(ShoppingCartService.class);
+        UserService userService = (UserService)
+                injector.getInstance(UserService.class);
+        System.out.println(shoppingCartService
+                .getByUser(userService.findByEmail("email2@com").get()));
+    }
+
+    private static void checkTicket() {
+        ShoppingCartService shoppingCartService = (ShoppingCartService)
+                injector.getInstance(ShoppingCartService.class);
+        MovieSessionService movieSessionService = (MovieSessionService)
+                injector.getInstance(MovieSessionService.class);
+        UserService userService = (UserService)
+                injector.getInstance(UserService.class);
+        MovieSession movieSession = new MovieSession();
+        CinemaHallService cinemaHallService = (CinemaHallService)
+                injector.getInstance(CinemaHallService.class);
+        movieSession.setCinemaHall(cinemaHallService.getAll().get(0));
+        MovieService movieService = (MovieService) injector.getInstance(MovieService.class);
+        Movie movie = movieService.getAll().get(0);
+        movieSession.setMovie(movie);
+        movieSession.setShowTime(LocalDateTime.of(LocalDate.now(), LocalTime.of(14, 30)));
+        movieSessionService.add(movieSession);
+        shoppingCartService.addSession(movieSession,
+                userService.findByEmail("email2@com").get());
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            Query<Ticket> query = session
+                    .createQuery("from Ticket", Ticket.class);
+            System.out.println(query.list());
+        } catch (Exception e) {
+            throw new DataProcessingException("Can't get "
+                    + "avalibal tickets from db", e);
+        }
     }
 }
---
+++ src/main/java/com/cinema/dao/impl/ShoppingCartDaoImpl.java
@@ -0,0 +1,70 @@
+package com.cinema.dao.impl;
+
+import com.cinema.dao.ShoppingCartDao;
+import com.cinema.exeption.DataProcessingException;
+import com.cinema.lib.Dao;
+import com.cinema.model.ShoppingCart;
+import com.cinema.model.User;
+import com.cinema.util.HibernateUtil;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.hibernate.query.Query;
+
+@Dao
+public class ShoppingCartDaoImpl implements ShoppingCartDao {
+    @Override
+    public ShoppingCart add(ShoppingCart shoppingCart) {
+        Transaction transaction = null;
+        Session session = null;
+        try {
+            session = HibernateUtil.getSessionFactory().openSession();
+            transaction = session.beginTransaction();
+            session.save(shoppingCart);
+            transaction.commit();
+            return shoppingCart;
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new DataProcessingException("Can`t insert shopping cart to db", e);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+    }
+
+    @Override
+    public ShoppingCart getByUser(User user) {
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            Query<ShoppingCart> query = session
+                    .createQuery("from ShoppingCart where id = :userId", ShoppingCart.class);
+            query.setParameter("userId", user.getId());
+            return query.uniqueResult();
+        } catch (Exception e) {
+            throw new DataProcessingException("Can't get "
+                    + "shopping cart by the user from db", e);
+        }
+    }
+
+    @Override
+    public void update(ShoppingCart shoppingCart) {
+        Transaction transaction = null;
+        Session session = null;
+        try {
+            session = HibernateUtil.getSessionFactory().openSession();
+            transaction = session.beginTransaction();
+            session.update(shoppingCart);
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new DataProcessingException("Can't update shopping cart in db", e);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+    }
+}
